01888ebaea55d93452f564e50b65811701ca7a9d
alkovachevich <alkovachevich@gmail.com>
2026-03-25 19:13:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-    const SUPABASE_URL = "https://rqtqimjenotjspqumeni.supabase.co";
-    const SUPABASE_ANON_KEY = "sb_publishable_LOzTBbVK8tg6kDOrO8AcrQ_j52hzXTf";
-    const GOOGLE_BOOKS_API_KEY = "AIzaSyAisvc1YIhHWofTe45-ESHF0JVp9t92Oys";
-    const TMDB_API_KEY = "fc8eab333882a74fe8c8a633e4676d98";
-
-    const supabaseClient = supabase.createClient(
-      SUPABASE_URL,
-      SUPABASE_ANON_KEY
-    );
+   
 
     const translations = {
       en: {
