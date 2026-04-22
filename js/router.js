797a60605bbc27ee8577ad868b0dc6d59e5cc633
alkovachevich <alkovachevich@gmail.com>
2026-04-22 20:51:45 +0200

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,37 +1,110 @@
-import { setState, state } from "./state.js";
 import { ROUTES } from "./config.js";
+import { setRoute, state } from "./state.js";
 
+/* =========================
+   HELPERS
+========================= */
 
-const validRoutes = new Set(Object.values(ROUTES));
+function parseQuery(search = "") {
+  const params = {};
+  const urlParams = new URLSearchParams(search);
 
+  for (const [key, value] of urlParams.entries()) {
+    params[key] = value;
+  }
+
+  return params;
+}
+
+function buildQuery(params = {}) {
+  const urlParams = new URLSearchParams();
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined && value !== null && value !== "") {
+      urlParams.set(key, value);
+    }
+  });
+
+  const queryString = urlParams.toString();
+  return queryString ? ?${queryString} : "";
+}
+
+/* =========================
+   ROUTE MATCHING
+========================= */
+
+function resolveRoute(pathname) {
+  // простое сопоставление путей
+  switch (pathname) {
+    case ROUTES.HOME:
+      return ROUTES.HOME;
+
+    case ROUTES.CATEGORIES:
+      return ROUTES.CATEGORIES;
+
+    case ROUTES.CATEGORY_LIBRARY:
+      return ROUTES.CATEGORY_LIBRARY;
+
+    case ROUTES.SEARCH:
+      return ROUTES.SEARCH;
+
+    case ROUTES.CARD:
+      return ROUTES.CARD;
+
+    case ROUTES.UNIVERSES:
+      return ROUTES.UNIVERSES;
+
+    case ROUTES.UNIVERSE_DETAILS:
+      return ROUTES.UNIVERSE_DETAILS;
+
+    case ROUTES.SETTINGS:
+      return ROUTES.SETTINGS;
+
+    case ROUTES.GUEST:
+      return ROUTES.GUEST;
+
+    default:
+      return ROUTES.HOME;
+  }
+}
+
+/* =========================
+   NAVIGATION
+========================= */
+
+export function navigate(route, params = {}) {
+  const path = route;
+  const query = buildQuery(params);
+
+  const url = ${path}${query};
+
+  if (window.location.pathname + window.location.search !== url) {
+    history.pushState({}, "", url);
+  }
+
+  setRoute(route, params);
+}
+
+/* =========================
+   INIT ROUTER
+========================= */
 
 export function initRouter() {
   window.addEventListener("popstate", syncRouteFromLocation);
   syncRouteFromLocation();
 }
 
-
-export function navigate(path) {
-  const nextPath = validRoutes.has(path) ? path : ROUTES.HOME;
-  if (window.location.pathname !== nextPath) {
-    history.pushState({}, "", nextPath);
-  }
-  setState({ route: nextPath });
-}
-
+/* =========================
+   SYNC
+========================= */
 
 function syncRouteFromLocation() {
-  const path = validRoutes.has(window.location.pathname)
-    ? window.location.pathname
-    : ROUTES.HOME;
+  const { pathname, search } = window.location;
 
+  const route = resolveRoute(pathname);
+  const params = parseQuery(search);
 
-  if (window.location.pathname !== path) {
-    history.replaceState({}, "", path);
-  }
-
-
-  if (state.route !== path) {
-    setState({ route: path });
+  if (state.route !== route || JSON.stringify(state.routeParams) !== JSON.stringify(params)) {
+    setRoute(route, params);
   }
 }
