8a9ca262edcbb7f23daa2d3c4823f65f71d881e3
alkovachevich <alkovachevich@gmail.com>
2026-04-23 17:10:47 +0200

    Remove GOOGLE_BOOKS_API_KEY from config.js
    Removed Google Books API key from configuration.

--- js/config.js
+++ js/config.js
@@ -2,7 +2,6 @@ export const APP_NAME = "Plamut";
 
 export const SUPABASE_URL = "https://rqtqimjenotjspqumeni.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_LOzTBbVK8tg6kDOrO8AcrQ_j52hzXTf";
-export const GOOGLE_BOOKS_API_KEY = "AIzaSyAisvc1YIhHWofTe45-ESHF0JVp9t92Oys";
 export const TMDB_API_KEY = "fc8eab333882a74fe8c8a633e4676d98";
 
 export const API_ENDPOINTS = {
